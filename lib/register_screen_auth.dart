fea6e3cf449b20f12452a909abafc78e96703d28
Ismail Hossain <hossain.eee042@gmail.com>
2023-09-14 01:30:58 +0600

    user registration has been done

---
+++ lib/register_screen_auth.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({Key? key}) : super(key: key);
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  //bool inProgress = false;
+
+  register() {
+    FirebaseAuth.instance
+        .createUserWithEmailAndPassword(
+            email: emailController.text.trim(),
+            password: passwordController.text)
+        .then((value) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Registration Successfull"),
+        ),
+      );
+      //clear the cotroller
+      emailController.clear();
+      passwordController.clear();
+    }).onError((error, stackTrace) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Registration Failed! Tray again"),
+        ),
+      );
+    });
+
+    //using getx
+/*     FirebaseAuth.instance
+        .createUserWithEmailAndPassword(
+            email: emailController.text, password: passwordController.text)
+        .then((value) {
+      Get.showSnackbar(
+        const GetSnackBar(
+          title: 'User registered successfully',
+          message: ' ',
+          backgroundColor: Colors.green,
+          duration: Duration(seconds: 2),
+        ),
+      );
+      //clear the cotroller
+      emailController.clear();
+      passwordController.clear();
+      // Get.to(LoginScreen)
+    }).onError((error, stackTrace) {
+      Get.showSnackbar(
+        const GetSnackBar(
+          title: 'Registration Failed',
+          message: ' ',
+          backgroundColor: Colors.redAccent,
+          duration: Duration(seconds: 2),
+        ),
+      );
+    }); */
+  }
+
+  // connectivity
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Registration Screen'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormField(
+                  controller: emailController,
+                  validator: (String? value) {
+                    if (value?.trim()?.isEmpty ?? true) {
+                      return 'Enter email';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                      hintText: 'Email', border: OutlineInputBorder()),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  controller: passwordController,
+                  validator: (String? value) {
+                    if (value?.trim()?.isEmpty ?? true) {
+                      return 'Enter password';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                      hintText: 'Password', border: OutlineInputBorder()),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      if (formKey.currentState!.validate()) {
+                        register();
+                      }
+                    },
+                    child: const Text('Register'))
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
